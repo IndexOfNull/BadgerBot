2a9f3f64c3e86c357bf3db3b450346863e537b25
IndexOfNull <21084775+IndexOfNull@users.noreply.github.com>
2020-05-05 15:23:43 -0500

    change confirmation system and add warning param

--- utils/funcs.py
+++ utils/funcs.py
@@ -20,12 +20,15 @@ def discord_obj_tostring(obj):
 
 #decorator to force commands to get confirmation from the invoking user
 class ConfirmationFailed(Exception): pass
-def require_confirmation():
+def require_confirmation(*, warning=None):
     def wrapper(coro):
         @functools.wraps(coro)
         async def wrapped(cog, ctx, *args, **kwargs):
             # Some fancy boo stuff
-            msg = await ctx.send(ctx.responses['verify_message'])
+            msg = ctx.responses['verify_message']
+            if warning:
+                msg += "\n**WARNING: " + warning + "**" #Needs a localization string
+            msg = await ctx.send(msg)
             allow, deny = ("✅", "❌")
             await msg.add_reaction(allow)
             await msg.add_reaction(deny)
@@ -34,15 +37,17 @@ def require_confirmation():
                 return user == ctx.author and (str(reaction.emoji) == allow or str(reaction.emoji) == deny)
 
             try:
-                reaction, user = await cog.bot.wait_for('reaction_add', timeout=20, check=check)
+                reaction, user = await ctx.bot.wait_for('reaction_add', timeout=20, check=check)
                 if str(reaction.emoji) == deny:
                     await msg.edit(content=ctx.responses['verify_canceled'])
+                    await msg.delete(delay=5)
                     raise ConfirmationFailed("The confirmation was canceled")
             except asyncio.TimeoutError:
                 await msg.edit(content=ctx.responses['verify_timeout'])
                 raise ConfirmationFailed("The confirmation request timed out")
             else:
                 await msg.edit(content=ctx.responses['verify_confirmed'])
+                await msg.delete(delay=5)
                 return await coro(cog, ctx, *args, **kwargs)
         return wrapped
     return wrapper
