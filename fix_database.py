9433b87c9827c3305438256da4ce7440430fa94a
IndexOfNull <21084775+IndexOfNull@users.noreply.github.com>
2021-04-11 00:33:16 -0500

    add database helper script

---
+++ fix_database.py
@@ -0,0 +1,12 @@
+import run
+import sqlalchemy as sa
+
+engine_uri = run.config['db_engine_uri']
+engine = sa.create_engine(engine_uri)
+
+connection = engine.connect()
+
+trans = connection.begin()
+connection.execute("ALTER TABLE `badges` CHANGE `text` `icon` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;")
+connection.execute("ALTER TABLE `badges` ADD `levels` INT NOT NULL DEFAULT '0' AFTER `icon`;")
+trans.commit()
